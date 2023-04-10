02d29227f503525165c3fafd18f9b1060fc35531
Lex Jansen <lexjansen@gmail.com>
2023-04-10 17:33:26 -0400

    small update

--- pharmasug-2023/programs/runbatch.cmd
+++ pharmasug-2023/programs/runbatch.cmd
@@ -8,10 +8,10 @@ if exist ..\definexml\*result*.html del ..\definexml\*result*.html
 
 
 %SAScmd% 01_create_sourcemetadata_from_definexml.sas %SASconfig%
-REM %SAScmd% 02_request_api_bc_latest.sas %SASconfig%
-REM %SAScmd% 02_request_api_sdtm_latest.sas %SASconfig%
-REM %SAScmd% 03_request_api_ct.sas %SASconfig%
-REM %SAScmd% 03_request_api_sdtm_domains.sas %SASconfig%
+%SAScmd% 02_request_api_bc_latest.sas %SASconfig%
+%SAScmd% 02_request_api_sdtm_latest.sas %SASconfig%
+%SAScmd% 03_request_api_ct.sas %SASconfig%
+%SAScmd% 03_request_api_sdtm_domains.sas %SASconfig%
 %SAScmd% 04_create_vlm_from_sdtm_specializations.sas %SASconfig%
 %SAScmd% 05_create_ct_metadata.sas %SASconfig%
 %SAScmd% 06_create_definexml_from_source.sas %SASconfig%
